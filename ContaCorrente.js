0916dc6b1349c459967590b3fd177972bfeb1c79
Mairon Azevedo <maironazevedo@id.uff.br>
2021-05-12 20:27:49 -0300

    Exporting and importing modules

---
+++ ContaCorrente.js
@@ -0,0 +1,16 @@
+class ContaCorrente {
+    agencia;
+    #saldo = 0;
+
+    sacar(valor) {
+        if (this.#saldo >= valor) {
+            this.#saldo -= valor;
+        }
+    } 
+
+    depositar(valor) {
+        if(valor > 0) {
+            this.#saldo += valor;
+        }
+    } 
+}
--- index.js
+++ index.js
@@ -1,25 +1,3 @@
-class Cliente {
-    nome;
-    cpf;
-}
-
-class ContaCorrente {
-    agencia;
-    #saldo;
-
-    sacar(valor) {
-        if (this.#saldo >= valor) {
-            this.#saldo -= valor;
-        }
-    } 
-
-    depositar(valor) {
-        if(valor > 0) {
-            this.saldo += valor;
-        }
-    } 
-}
-
 const cliente1 = new Cliente();
 cliente1.nome = "Ricardo";
 cliente1.cpf = "11122233309";
